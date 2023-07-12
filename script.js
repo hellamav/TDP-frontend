ed3c86b901035123c4efcbbdb62d49c3cbb51b14
Hella <hel@gmail.com>
2023-07-12 14:04:27 +0100

    added fizzbuzz

--- script.js
+++ script.js
@@ -79,3 +79,14 @@ let animalList = [
 }
 }
   
+for (let num = 1; num <=100; num++) {
+  let output = "";
+  if (num % 3 === 0) {
+      output += "Fizz";
+  }
+
+  if (num % 5 === 0){
+      output += "Buzz";
+  }
+  console.log(output || num);
+}
